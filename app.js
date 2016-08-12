7a345a3b37f9ec2c911930bf0b02a0accc2f6764
Prima Aulia Gusta <primaulia@gmail.com>
2016-08-12 15:24:36 +0800

    just wanna see travis to check my code

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.set('port', (process.env.PORT || 7000));
 
 app.listen(app.get('port'), function() {
   console.log('My express server is running at localhost', app.get('port'));
-});
+}); 
 
 module.exports = app;
